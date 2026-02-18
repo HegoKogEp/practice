0049830dbafe41fa30cf66d7806a26b3686ae897
Виталий Буч <bucvitalij7@gmail.com>
2026-02-18 22:07:29 +0700

    Task 1

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
     namespace = "ci.nsu.moble.main"
     compileSdk = 36
+
 
     defaultConfig {
         applicationId = "ci.nsu.moble.main"
@@ -15,6 +17,10 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures{
+        compose = true
     }
 
     buildTypes {
@@ -30,15 +36,36 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
 
-    implementation("com.android.support:appcompat-v7:28.0.0")
+    implementation(platform("androidx.compose:compose-bom:2024.10.00"))
+
+    // Основные библиотеки Compose
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+
+    // Activity с поддержкой Compose (нужно для setContent)
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
+    implementation("androidx.compose.material3:material3-adaptive-navigation-suite")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.09.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
+    // Для preview в Android Studio
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.10.0")
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.9")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("com.android.support.test:runner:1.0.2")
     androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
